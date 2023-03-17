6a87322b6bd6f719036c843630823b060e460223
Souma <553469159@qq.com>
2023-03-18 02:25:19 +0800

    绝OP

--- 必装/souma拓展运行库.js
+++ 必装/souma拓展运行库.js
@@ -2,36 +2,49 @@ if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_tim
   let isInit = false;
   const prevent = { partyMark: false };
   const jobs = {
-    1: { 简体: "剑术", 繁体: "劍術", 单字: "剑" },
-    2: { 简体: "格斗", 繁体: "格鬥", 单字: "格" },
-    3: { 简体: "斧术", 繁体: "斧術", 单字: "斧" },
-    4: { 简体: "枪术", 繁体: "槍術", 单字: "矛" },
-    5: { 简体: "弓箭", 繁体: "弓箭", 单字: "弓" },
-    6: { 简体: "幻术", 繁体: "幻術", 单字: "幻" },
-    7: { 简体: "咒术", 繁体: "咒術", 单字: "咒" },
-    19: { 简体: "骑士", 繁体: "騎士", 单字: "骑" },
-    20: { 简体: "武僧", 繁体: "武僧", 单字: "僧" },
-    21: { 简体: "战士", 繁体: "戰士", 单字: "战" },
-    22: { 简体: "龙骑", 繁体: "龍騎", 单字: "龙" },
-    23: { 简体: "诗人", 繁体: "詩人", 单字: "诗" },
-    24: { 简体: "白魔", 繁体: "白魔", 单字: "白" },
-    25: { 简体: "黑魔", 繁体: "黑魔", 单字: "黑" },
-    26: { 简体: "秘术", 繁体: "秘術", 单字: "秘" },
-    27: { 简体: "召唤", 繁体: "召喚", 单字: "召" },
-    28: { 简体: "学者", 繁体: "學者", 单字: "学" },
-    29: { 简体: "双剑", 繁体: "雙劍", 单字: "双" },
-    30: { 简体: "忍者", 繁体: "忍者", 单字: "忍" },
-    31: { 简体: "机工", 繁体: "機工", 单字: "机" },
-    32: { 简体: "暗骑", 繁体: "暗騎", 单字: "暗" },
-    33: { 简体: "占星", 繁体: "占星", 单字: "占" },
-    34: { 简体: "武士", 繁体: "武士", 单字: "侍" },
-    35: { 简体: "赤魔", 繁体: "赤魔", 单字: "赤" },
-    36: { 简体: "青魔", 繁体: "青魔", 单字: "青" },
-    37: { 简体: "绝枪", 繁体: "絕槍", 单字: "枪" },
-    38: { 简体: "舞者", 繁体: "舞者", 单字: "舞" },
-    39: { 简体: "钐镰", 繁体: "釤鐮", 单字: "钐" },
-    40: { 简体: "贤者", 繁体: "賢者", 单字: "贤" },
+    0: { 简体: "冒险者", 繁体: "冒險者", 单字: "冒", 双字: "冒险", role: "none", LB: "none", job: "ADV" },
+    1: { 简体: "剑术师", 繁体: "劍術師", 单字: "剑", 双字: "剑术", role: "tank", LB: "tank", job: "GLA" },
+    2: { 简体: "格斗家", 繁体: "格鬥家", 单字: "格", 双字: "格斗", role: "dps", LB: "melee", job: "PGL" },
+    3: { 简体: "斧术师", 繁体: "斧術師", 单字: "斧", 双字: "斧术", role: "tank", LB: "tank", job: "MRD" },
+    4: { 简体: "枪术师", 繁体: "槍術師", 单字: "枪", 双字: "枪术", role: "dps", LB: "melee", job: "LNC" },
+    5: { 简体: "弓箭手", 繁体: "弓箭手", 单字: "弓", 双字: "弓箭", role: "dps", LB: "ranged", job: "ARC" },
+    6: { 简体: "幻术师", 繁体: "幻術師", 单字: "幻", 双字: "幻术", role: "healer", LB: "healer", job: "CNJ" },
+    7: { 简体: "咒术师", 繁体: "咒術師", 单字: "咒", 双字: "咒术", role: "dps", LB: "caster", job: "THM" },
+    8: { 简体: "刻木匠", 繁体: "刻木匠", 单字: "木", 双字: "刻木", role: "crafter", LB: "melee", job: "CRP" },
+    9: { 简体: "锻铁匠", 繁体: "鍛鐵匠", 单字: "锻", 双字: "锻铁", role: "crafter", LB: "melee", job: "BSM" },
+    10: { 简体: "铸甲匠", 繁体: "鑄甲匠", 单字: "甲", 双字: "铸甲", role: "crafter", LB: "melee", job: "ARM" },
+    11: { 简体: "雕金匠", 繁体: "雕金匠", 单字: "雕", 双字: "雕金", role: "crafter", LB: "melee", job: "GSM" },
+    12: { 简体: "制革匠", 繁体: "製革匠", 单字: "革", 双字: "制革", role: "crafter", LB: "melee", job: "LTW" },
+    13: { 简体: "裁衣匠", 繁体: "裁衣匠", 单字: "裁", 双字: "裁衣", role: "crafter", LB: "melee", job: "WVR" },
+    14: { 简体: "炼金术士", 繁体: "煉金術士", 单字: "炼", 双字: "炼金", role: "crafter", LB: "melee", job: "ALC" },
+    15: { 简体: "烹调师", 繁体: "烹調師", 单字: "烹", 双字: "烹调", role: "crafter", LB: "melee", job: "CUL" },
+    16: { 简体: "采矿工", 繁体: "采礦工", 单字: "矿", 双字: "采矿", role: "gatherer", LB: "melee", job: "MIN" },
+    17: { 简体: "园艺工", 繁体: "園藝工", 单字: "园", 双字: "园艺", role: "gatherer", LB: "melee", job: "BTN" },
+    18: { 简体: "捕鱼人", 繁体: "捕魚人", 单字: "鱼", 双字: "捕鱼", role: "gatherer", LB: "melee", job: "FSH" },
+    19: { 简体: "骑士", 繁体: "騎士", 单字: "骑", 双字: "骑士", role: "tank", LB: "tank", job: "PLD" },
+    20: { 简体: "武僧", 繁体: "武僧", 单字: "僧", 双字: "武僧", role: "dps", LB: "melee", job: "MNK" },
+    21: { 简体: "战士", 繁体: "戰士", 单字: "战", 双字: "战士", role: "tank", LB: "tank", job: "WAR" },
+    22: { 简体: "龙骑士", 繁体: "龍騎士", 单字: "龙", 双字: "龙骑", role: "dps", LB: "melee", job: "DRG" },
+    23: { 简体: "吟游诗人", 繁体: "吟遊詩人", 单字: "诗", 双字: "诗人", role: "dps", LB: "ranged", job: "BRD" },
+    24: { 简体: "白魔法师", 繁体: "白魔法師", 单字: "白", 双字: "白魔", role: "healer", LB: "healer", job: "WHM" },
+    25: { 简体: "黑魔法师", 繁体: "黑魔法師", 单字: "黑", 双字: "黑魔", role: "dps", LB: "caster", job: "BLM" },
+    26: { 简体: "秘术师", 繁体: "秘術師", 单字: "秘", 双字: "秘术", role: "dps", LB: "caster", job: "ACN" },
+    27: { 简体: "召唤师", 繁体: "召喚師", 单字: "召", 双字: "召唤", role: "dps", LB: "caster", job: "SMN" },
+    28: { 简体: "学者", 繁体: "學者", 单字: "学", 双字: "学者", role: "healer", LB: "healer", job: "SCH" },
+    29: { 简体: "双剑师", 繁体: "雙劍師", 单字: "双", 双字: "双剑", role: "dps", LB: "melee", job: "ROG" },
+    30: { 简体: "忍者", 繁体: "忍者", 单字: "忍", 双字: "忍者", role: "dps", LB: "melee", job: "NIN" },
+    31: { 简体: "机工士", 繁体: "機工士", 单字: "机", 双字: "机工", role: "dps", LB: "ranged", job: "MCH" },
+    32: { 简体: "暗黑骑士", 繁体: "暗黑騎士", 单字: "暗", 双字: "暗骑", role: "tank", LB: "tank", job: "DRK" },
+    33: { 简体: "占星术士", 繁体: "占星術士", 单字: "占", 双字: "占星", role: "healer", LB: "healer", job: "AST" },
+    34: { 简体: "武士", 繁体: "武士", 单字: "武", 双字: "武士", role: "dps", LB: "melee", job: "SAM" },
+    35: { 简体: "赤魔法师", 繁体: "赤魔法師", 单字: "赤", 双字: "赤魔", role: "dps", LB: "caster", job: "RDM" },
+    36: { 简体: "青魔法师", 繁体: "青魔法師", 单字: "青", 双字: "青魔", role: "dps", LB: "caster", job: "BLU" },
+    37: { 简体: "绝枪战士", 繁体: "絕槍戰士", 单字: "绝", 双字: "绝枪", role: "tank", LB: "tank", job: "GNB" },
+    38: { 简体: "舞者", 繁体: "舞者", 单字: "舞", 双字: "舞者", role: "dps", LB: "ranged", job: "DNC" },
+    39: { 简体: "钐镰客", 繁体: "釤鐮客", 单字: "钐", 双字: "钐镰", role: "dps", LB: "melee", job: "RPR" },
+    40: { 简体: "贤者", 繁体: "賢者", 单字: "贤", 双字: "贤者", role: "healer", LB: "healer", job: "SGE" },
   };
+
   const sleep = (ms) => new Promise((_resolve, reject) => setTimeout(reject, ms));
   const waitFor = async function waitFor(f) {
     while (!f()) await sleep(200);
@@ -66,6 +79,7 @@ if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_tim
     pos8: ["上", "右上", "右", "右下", "下", "左下", "左", "左上"],
   };
   function ifMissing() {
+    console.trace("缺少参数");
     throw "缺少参数";
   }
   function markTypeLegalityCheck(markTypeStr) {
@@ -104,13 +118,19 @@ if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_tim
     return data.party.details.find((v) => v.name === name).id;
   }
   function getJobNameByRP(data = ifMissing(), rp = ifMissing(), type = "简体") {
-    return jobs?.[data.party.details.find((v) => v.name === getNameByRp(data, rp))?.job]?.[type] ?? "";
+    return jobs?.[data.party.details.find((v) => v.name === getNameByRp(data, rp))?.job]?.[type];
   }
   function getJobNameByHexId(data = ifMissing(), hexId = ifMissing(), type = "简体") {
-    return jobs?.[data.party.details.find((v) => v.name === getNameByHexId(data, hexId))?.job]?.[type] ?? "";
+    return jobs?.[data.party.details.find((v) => v.name === getNameByHexId(data, hexId))?.job]?.[type];
   }
   function getJobNameByName(data = ifMissing(), name = ifMissing(), type = "简体") {
-    return jobs?.[data.party.details.find((v) => v.name === name)?.job]?.[type] ?? "";
+    return jobs?.[data.party.details.find((v) => v.name === name)?.job]?.[type];
+  }
+  function getRoleByName(data = ifMissing(), name = ifMissing()) {
+    return jobs?.[data.party.details.find((v) => v.name === name)?.job]?.role;
+  }
+  function getLBByName(data = ifMissing(), name = ifMissing()) {
+    return jobs?.[data.party.details.find((v) => v.name === name)?.job]?.LB;
   }
   function getHexIdByRp(data = ifMissing(), rp = ifMissing()) {
     return data.party.partyIds_[data.party.partyNames_.indexOf(getNameByRp(data, rp))];
@@ -133,7 +153,7 @@ if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_tim
   function getpartyNamesGroupMeleeDps(data = ifMissing()) {
     return [getNameByRp(data, "D1"), getNameByRp(data, "D2")];
   }
-  function getpartyNamesGroupRangeDps(data = ifMissing()) {
+  function getpartyNamesGroupRangedDps(data = ifMissing()) {
     return [getNameByRp(data, "D3"), getNameByRp(data, "D4")];
   }
   function sortPartyHexIdsByRpRule(data = ifMissing(), partyHexIdsArr = ifMissing(), rpRuleArr = ifMissing()) {
@@ -239,7 +259,7 @@ if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_tim
             if (typeof v.p.ActorID === "string") v.p.ActorID = parseInt(v.p.ActorID, 16);
             if (Number.isNaN(v.p.ActorID)) throw "v.p.ActorID is not a number";
             if (/undefined/.test(v.p.MarkType)) throw "MarkType has undefined";
-            const decId = v.p.ActorID;
+            // const decId = v.p.ActorID;
             const hexId = v.p.ActorID.toString(16).toUpperCase();
             v.debugHexId = hexId;
             v.debugName = getNameByHexId(data, hexId);
@@ -409,13 +429,15 @@ if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_tim
           getJobNameByRP,
           getJobNameByHexId,
           getJobNameByName,
+          getRoleByName,
+          getLBByName,
           getpartyNamesGroupH1,
           getpartyNamesGroupH2,
           getpartyNamesGroupTank,
           getpartyNamesGroupHealer,
           getpartyNamesGroupAllDps,
           getpartyNamesGroupMeleeDps,
-          getpartyNamesGroupRangeDps,
+          getpartyNamesGroupRangedDps,
           loop,
           sleep,
           waitFor,
@@ -484,12 +506,14 @@ if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_tim
       },
     ],
   });
-  addOverlayListener("PartyChanged", (e) => {
-    if (soumaRuntimeJSData === null) setTimeout(() => createMyParty(e.party), 500);
-    else createMyParty(e.party);
-  });
-  addOverlayListener("ChangeZone", () => placeReset());
-  addOverlayListener("BroadcastMessage", handleBroadcastMessage);
+  if (!/config\.html/.test(location.href)) {
+    addOverlayListener("PartyChanged", (e) => {
+      if (soumaRuntimeJSData === null) setTimeout(() => createMyParty(e.party), 500);
+      else createMyParty(e.party);
+    });
+    addOverlayListener("ChangeZone", () => placeReset());
+    addOverlayListener("BroadcastMessage", handleBroadcastMessage);
+  }
   function handleBroadcastMessage(msg) {
     if (msg.source === "soumaRuntimeJS") {
       soumaRuntimeJSData = msg.msg.party;
