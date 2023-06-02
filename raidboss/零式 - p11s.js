f8e44dff761b52a01d37afdfde9fe8fb7e5e1313
Souma <553469159@qq.com>
2023-06-03 03:06:06 +0800

    能用就行

---
+++ raidboss/零式 - p11s.js
@@ -0,0 +1,224 @@
+const firstMarker = parseInt("", 16);
+const headmarkers = {};
+const getHeadmarkerId = (data, matches) => {
+  if (data.souma.decOffset === undefined) data.souma.decOffset = parseInt(matches.id, 16) - firstMarker;
+  return (parseInt(matches.id, 16) - data.souma.decOffset).toString(16).toUpperCase().padStart(4, "0");
+};
+const startsUsingStrings = {};
+const { getRpByName, doTextCommand, doQueueActions, doQueueActionsDebug, sortMatchesBy, sortMatchesByFill, getClearMarkQueue, orientation4, deepClone, mark } =
+  Util.souma;
+Options.Triggers.push({
+  id: "SoumaAnabaseiosTheEleventhCircleSavage",
+  zoneId: ZoneId.AnabaseiosTheEleventhCircleSavage,
+  config: [],
+  initData: () => {
+    if (!location.href.includes("raidemulator.html")) {
+      callOverlayHandler({
+        call: "PostNamazu",
+        c: "DoQueueActions",
+        p: JSON.stringify([
+          { c: "stop", p: "P11S Souma Public Queue .*" },
+          // { c: "DoWaymarks", p: "load", d: 3000 },
+          { c: "DoTextCommand", p: "/mk off <1>", d: 3000 },
+          { c: "DoTextCommand", p: "/mk off <2>" },
+          { c: "DoTextCommand", p: "/mk off <3>" },
+          { c: "DoTextCommand", p: "/mk off <4>" },
+          { c: "DoTextCommand", p: "/mk off <5>" },
+          { c: "DoTextCommand", p: "/mk off <6>" },
+          { c: "DoTextCommand", p: "/mk off <7>" },
+          { c: "DoTextCommand", p: "/mk off <8>" },
+        ]),
+      });
+    }
+    return {
+      souma: {
+        decOffset: undefined,
+        phase: 1,
+      },
+    };
+  },
+  triggers: [
+    {
+      id: "P11S Souma Headmarker Tracker",
+      type: "HeadMarker",
+      netRegex: {},
+      condition: (data) => data.souma.decOffset === undefined,
+      run: (data, matches) => getHeadmarkerId(data, matches),
+    },
+    {
+      id: "P11S Souma エウノミアー",
+      type: "StartsUsing",
+      netRegex: { id: "822B", capture: true },
+      response: Responses.bigAoe(),
+    },
+    {
+      id: "P11S Souma 连线",
+      type: "Tether",
+      netRegex: { id: "00F9", capture: true },
+      condition: Conditions.targetIsYou(),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: { text: { en: "连线" } },
+    },
+    {
+      id: "P11S Souma 死刑",
+      type: "StartsUsing",
+      netRegex: { id: "822D" },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: "P11S Souma 连线光",
+      type: "StartsUsing",
+      netRegex: { id: "87D3" },
+      alertText: (data, _, output) => output.text(),
+      durationSeconds: 12,
+      outputStrings: {
+        text: { en: "集合 => 外4人分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 连线暗",
+      type: "StartsUsing",
+      netRegex: { id: "87D4" },
+      alertText: (data, _, output) => output.text(),
+      durationSeconds: 12,
+      outputStrings: {
+        text: { en: "外 => 内X字分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 普通光",
+      type: "StartsUsing",
+      netRegex: { id: "81E6" },
+      alertText: (data, _, output) => output.text(),
+      durationSeconds: 12,
+      outputStrings: {
+        text: { en: "外八方散开 => 内4人分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 普通暗",
+      type: "StartsUsing",
+      netRegex: { id: "81E7" },
+      alertText: (data, _, output) => output.text(),
+      durationSeconds: 12,
+      outputStrings: {
+        text: { en: "八方散开 + 小月环2人分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 不知道什么光",
+      type: "StartsUsing",
+      netRegex: { id: "81EC" },
+      alertText: (data, _, output) => output.text(),
+      durationSeconds: 12,
+      outputStrings: {
+        text: { en: "两侧(大) => 4人分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 不知道什么暗",
+      type: "StartsUsing",
+      netRegex: { id: "81ED" },
+      alertText: (data, _, output) => output.text(),
+      durationSeconds: 12,
+      outputStrings: {
+        text: { en: "两侧 => 内X字分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 塔器Q光",
+      type: "StartsUsing",
+      netRegex: { id: "87B3" },
+      durationSeconds: 12,
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: { en: "两侧(大) => 4人分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 塔器Q暗",
+      type: "StartsUsing",
+      netRegex: { id: "87B4" },
+      durationSeconds: 12,
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: { en: "两侧(大) => 内X字分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 魔法阵光",
+      type: "StartsUsing",
+      netRegex: { id: "820D" },
+      infoText: (data, _, output) => (data.souma.phase === 1 ? output.text() : output.text2()),
+      durationSeconds: 6,
+      outputStrings: {
+        text: { en: "找光门" },
+        text2: { en: "找暗门" },
+      },
+    },
+    {
+      id: "P11S Souma 魔法阵暗",
+      type: "StartsUsing",
+      netRegex: { id: "820E" },
+      infoText: (data, _, output) => (data.souma.phase === 1 ? output.text() : output.text2()),
+      durationSeconds: 6,
+      outputStrings: {
+        text: { en: "找暗门" },
+        text2: { en: "找光门" },
+      },
+    },
+    {
+      id: "P11S Souma 这又是什么光",
+      type: "StartsUsing",
+      netRegex: { id: "8784" },
+      delaySeconds: 2.5,
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: { en: "钢铁 + 4人分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 这又是什么暗",
+      type: "StartsUsing",
+      netRegex: { id: "8785" },
+      delaySeconds: 2.5,
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: { en: "月环 => 内X字分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 魔法阵光2",
+      type: "StartsUsing",
+      netRegex: { id: "820F" },
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: { en: "找暗球" },
+      },
+    },
+    {
+      id: "P11S Souma 魔法阵暗2",
+      type: "StartsUsing",
+      netRegex: { id: "8210" },
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: { en: "找光球" },
+      },
+    },
+    {
+      id: "P11S Souma 冥河",
+      type: "StartsUsing",
+      netRegex: { id: "8217" },
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: { en: "集合分摊" },
+      },
+    },
+    {
+      id: "P11S Souma 分阶段",
+      type: "StartsUsing",
+      netRegex: { id: "8202" },
+      run: (data) => (data.souma.phase = 2),
+    },
+  ],
+});
---
+++ raidboss/零式 - p9s.js
@@ -0,0 +1,141 @@
+const firstMarker = parseInt("0221", 16);
+const headmarkers = {
+  m1: "009C",
+  m2: "009D",
+  m3: "009E",
+  m4: "009F",
+  m5: "00A0",
+  m6: "00A1",
+  m7: "00A2",
+  m8: "00A3",
+  lan: "0197",
+};
+const getHeadmarkerId = (data, matches) => {
+  if (data.souma.decOffset === undefined) data.souma.decOffset = parseInt(matches.id, 16) - firstMarker;
+  return (parseInt(matches.id, 16) - data.souma.decOffset).toString(16).toUpperCase().padStart(4, "0");
+};
+const { getRpByName, doTextCommand, doQueueActions, doQueueActionsDebug, sortMatchesBy, sortMatchesByFill, getClearMarkQueue, orientation4, deepClone, mark } =
+  Util.souma;
+// console.warn("p9s");
+Options.Triggers.push({
+  id: "SoumaAnabaseiosTheNinthCircleSavage",
+  zoneId: ZoneId.AnabaseiosTheNinthCircleSavage,
+  config: [
+    {
+      id: "麻将6824标点1234",
+      name: { en: "麻将6824标点1234" },
+      type: "select",
+      options: { en: { "开(正常模式)": "开", "关": "关", "开(本地标点)": "本地" } },
+      default: "关",
+    },
+  ],
+  initData: () => {
+    if (!location.href.includes("raidemulator.html")) {
+      callOverlayHandler({
+        call: "PostNamazu",
+        c: "DoQueueActions",
+        p: JSON.stringify([
+          { c: "stop", p: "P9S Souma Public Queue .*" },
+          // { c: "DoWaymarks", p: "load", d: 3000 },
+          { c: "DoTextCommand", p: "/mk off <1>", d: 3000 },
+          { c: "DoTextCommand", p: "/mk off <2>" },
+          { c: "DoTextCommand", p: "/mk off <3>" },
+          { c: "DoTextCommand", p: "/mk off <4>" },
+          { c: "DoTextCommand", p: "/mk off <5>" },
+          { c: "DoTextCommand", p: "/mk off <6>" },
+          { c: "DoTextCommand", p: "/mk off <7>" },
+          { c: "DoTextCommand", p: "/mk off <8>" },
+        ]),
+      });
+    }
+    return {
+      souma: {
+        decOffset: undefined,
+        stage: 1,
+        // element: undefined,
+        // markers: {
+        //   spider: [],
+        // },
+        // towers: [],
+        // towersMap: [],
+        // plumesStr: undefined,
+      },
+    };
+  },
+  triggers: [
+    {
+      id: "P9S Souma Headmarker Tracker",
+      type: "HeadMarker",
+      netRegex: {},
+      condition: (data) => data.souma.decOffset === undefined,
+      run: (data, matches) => getHeadmarkerId(data, matches),
+    },
+    {
+      id: "P9S Souma HeadMarker 麻将1",
+      type: "HeadMarker",
+      netRegex: {},
+      durationSeconds: 30,
+      alertText: (data, matches, output) => {
+        console.warn(matches)
+        if (data.souma.stage === 1) {
+          const id = getHeadmarkerId(data, matches);
+          // console.warn(id);
+          let res;
+          const isMark = ["开", "本地"].includes(data.triggerSetConfig.麻将6824标点1234);
+          const local = data.triggerSetConfig.麻将6824标点1234 === "本地";
+          switch (id) {
+            case headmarkers.m2:
+              res = "2麻 1火3塔";
+              if (isMark) mark(parseInt(matches.targetId, 16), "attack3", local);
+              break;
+            case headmarkers.m4:
+              res = "4麻 3火4塔";
+              if (isMark) mark(parseInt(matches.targetId, 16), "attack4", local);
+              break;
+            case headmarkers.m6:
+              res = "6麻 1塔3火";
+              if (isMark) mark(parseInt(matches.targetId, 16), "attack1", local);
+              break;
+            case headmarkers.m8:
+              res = "8麻 2塔4火";
+              if (isMark) mark(parseInt(matches.targetId, 16), "attack2", local);
+              break;
+            case headmarkers.lan:
+              // mark(parseInt(matches.targetId, 16), "stop1", local);
+              res = "篮球 找BOSS";
+              break;
+          }
+          if (matches.target === data.me) return res;
+        }
+      },
+    },
+    {
+      id: "P9S Souma 前方踢脚",
+      type: "StartsUsing",
+      netRegex: { id: ["8167", "8168"], capture: false },
+      infoText: "后后后",
+    },
+    {
+      id: "P9S Souma 后方踢脚",
+      type: "StartsUsing",
+      netRegex: { id: ["8169", "816A"], capture: false },
+      infoText: "前前前",
+    },
+    {
+      id: "P9S Souma 双击1",
+      type: "StartsUsing",
+      netRegex: { id: "8184", capture: false },
+      infoText: "双分摊",
+      run: (data) => (data.souma.stage = 2),
+      // run: (data, matches) => doTextCommand(`/p (冰+火) 遠離+分組`),
+    },
+    {
+      id: "P9S Souma 双击2",
+      type: "StartsUsing",
+      netRegex: { id: "8185", capture: false },
+      infoText: "靠近+八方",
+      run: (data) => (data.souma.stage = 2),
+      // run: (data, matches) => doTextCommand(`/p (冰+雷) 靠近+散開`),
+    },
+  ],
+});
