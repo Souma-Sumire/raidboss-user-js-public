79ca6ed91aeb6817f2511250559b0ccdf32c9eb9
Souma-Sumire <553469159@qq.com>
2023-08-07 13:57:50 +0800

    Create 高难通用 - 食物警察.js

---
+++ raidboss/高难通用 - 食物警察.js
@@ -0,0 +1,39 @@
+if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_timeline_only/.test(location.href)) {
+  const check = (effects, time) =>
+    effects
+      .map((e) =>
+        e.Effects ? (e.Effects.Timer < time * 60 ? `${e.Name}食物即将消失（${Math.floor(e.Effects.Timer / 60)}分钟)` : undefined) : `${e.Name}没吃食物`,
+      )
+      .filter(Boolean)
+      .join(",");
+
+  Options.Triggers.push({
+    zoneId: ZoneId.MatchAll,
+    id: "souma_top_hunger_police",
+    config: [
+      { id: "hunger_police_enable", name: { en: "开启" }, type: "checkbox", default: true },
+      { id: "hunger_police_time", name: { en: "报警阈值（分钟）" }, type: "string", default: "20" },
+      { id: "hunger_police_countdown", name: { en: "倒计时触发" }, type: "checkbox", default: true },
+      { id: "hunger_police_echo", name: { en: '"/e food"触发' }, type: "checkbox", default: true },
+    ],
+    triggers: [
+      {
+        id: "TOP hunger_police",
+        regex:
+          /^.{14} ChatLog 00:(?:00B9|0[12]39)::(?:距离战斗开始还有|Battle commencing in |戦闘開始まで)(?<cd>\d+)[^（]+（|^.{14} ChatLog 00:0038::(?<echo>food)$/i,
+        condition: (data, matches) =>
+          data.triggerSetConfig.hunger_police_enable ||
+          (matches.cd && data.triggerSetConfig.hunger_police_countdown) ||
+          (matches.echo && data.triggerSetConfig.hunger_police_echo),
+        promise: async (data) => {
+          const ids = data.party.partyIds_.length ? data.party.partyIds_.map((v) => parseInt(v, 16)) : undefined;
+          const combatants = (await callOverlayHandler({ call: "getCombatants", ids })).combatants.filter((v) => (ids ? ids.inclues(v) : v.Name === data.me));
+          const effects = combatants.map((v) => ({ Name: data.ShortName(v.Name), Effects: v.Effects.filter((e) => e.BuffID === 48)[0] }));
+          data.hunger_police_text = check(effects, Number(data.triggerSetConfig.hunger_police_time));
+        },
+        infoText: (data) => data.hunger_police_text,
+        soundVolume: 0,
+      },
+    ],
+  });
+}
