b25e70724beb4ebf7507dda97c125a37e44c301c
Souma-Sumire <553469159@qq.com>
2023-10-03 19:01:07 +0800

    1

--- raidboss/零式 - p12s.js
+++ raidboss/零式 - p12s.js
@@ -3083,29 +3083,29 @@ if (new URLSearchParams(location.search).get("alerts") !== "0" && !/raidboss_tim
           text: { en: "去南边" },
         },
       },
-      {
-        id: "P12S Souma 不确定的黑白塔",
-        comment: { en: "不保证一定正确" },
-        type: "StartsUsing",
-        netRegex: { id: "8343", capture: true },
-        suppressSeconds: 999,
-        promise: async (data, matches, output) => {
-          if (data.souma.pantheismUndetermined === undefined) return;
-          const combatants = (await callOverlayHandler({ call: "getCombatants", ids: [parseInt(matches.sourceId, 16)] })).combatants;
-          const tower = (parseFloat(combatants[0].PosX) < 100 ? { 白: "left", 黑: "right" } : { 白: "right", 黑: "left" })[
-            data.souma.pantheismUndetermined.color
-          ];
-          data.souma.hbtInfoText = output[tower + data.souma.pantheismUndetermined.round]();
-        },
-        infoText: (data) => data.souma.hbtInfoText,
-        run: (data) => delete data.souma.hbtInfoText,
-        outputStrings: {
-          left1: { en: "左1" },
-          left2: { en: "左等待 " },
-          right1: { en: "右1" },
-          right2: { en: "右等待" },
-        },
-      },
+      // {
+      //   id: "P12S Souma 不确定的黑白塔",
+      //   comment: { en: "不保证一定正确" },
+      //   type: "StartsUsing",
+      //   netRegex: { id: "8343", capture: true },
+      //   suppressSeconds: 999,
+      //   promise: async (data, matches, output) => {
+      //     if (data.souma.pantheismUndetermined === undefined) return;
+      //     const combatants = (await callOverlayHandler({ call: "getCombatants", ids: [parseInt(matches.sourceId, 16)] })).combatants;
+      //     const tower = (parseFloat(combatants[0].PosX) < 100 ? { 白: "left", 黑: "right" } : { 白: "right", 黑: "left" })[
+      //       data.souma.pantheismUndetermined.color
+      //     ];
+      //     data.souma.hbtInfoText = output[tower + data.souma.pantheismUndetermined.round]();
+      //   },
+      //   infoText: (data) => data.souma.hbtInfoText,
+      //   run: (data) => delete data.souma.hbtInfoText,
+      //   outputStrings: {
+      //     left1: { en: "左1" },
+      //     left2: { en: "左等待 " },
+      //     right1: { en: "右1" },
+      //     right2: { en: "右等待" },
+      //   },
+      // },
     ],
   });
 }
