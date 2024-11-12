137018eee1b519b9cc1fd3b0e48074db948515d7
Souma-Sumire <553469159@qq.com>
2024-11-13 00:04:09 +0800

    fix

--- raidboss/config面板转换.js
+++ raidboss/config面板转换.js
@@ -1,4 +1,4 @@
 if (/^file:\/\/.+\/config\/config\.html$/.test(location.href)) {
-    location.href = 'https://souma.diemoe.net/cactbot/ui/config/config.html';
+    location.href = 'http://souma.diemoe.net/cactbot/ui/config/config.html';
     console.log('已重定向至 souma 修改版 config');
 }
